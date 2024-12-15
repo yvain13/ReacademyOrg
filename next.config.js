d3f798d869a242bcf9f8002dcaebe169d36cb037
Tushar Mishra <tushar.x.mishra@gmail.com>
2024-12-15 12:18:14 -0500

    Sixth Deploy

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    appDir: false,
+  },
+}
+
+module.exports = nextConfig
